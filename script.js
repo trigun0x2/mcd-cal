1b24802fe139c4e9f2d19f125673968e43af0601
Jeffrey Tong <trigun0x2@gmail.com>
2014-12-19 00:15:32 -0500

    fixed for 0 days

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(function() {
   $(".days").on("input", null, null, function(){
     days = $(this).val();
-    price = days*8.95*2+5
+    if (days == 0){
+      price = 0;
+    }else{
+      price = days*8.95*2+5;
+    }
     $(".total-cost").text("$" + Math.round(price *100)/100);
   });
 });
